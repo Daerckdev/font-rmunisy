030b8af5018f93a8497e8d5ce0ec45c72add30bf
Daerckdev <darckflix@gmail.com>
2022-01-06 23:59:35 +0100

    added support for multiple files at time

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
-import fontforge, sys, os
-import argparse
+import fontforge, sys
+import argparse, pathlib
 
-parser = argparse.ArgumentParser()
-parser.add_argument('font_path', help='The path to the font to patch')
+parser = argparse.ArgumentParser(description="Delete unicode characters from a font using FontForge")
+parser.add_argument('fonts_path', type=pathlib.Path, nargs="+", help="The path to the fonts to patch")
+parser.add_argument('-i', dest="to_delete", required=True, type=pathlib.Path, help="The path of the file contain the unicodes to delete")
+parser.add_argument('-o', dest="output_dir", required=False, type=pathlib.Path, default=pathlib.Path("build"), help="Output Directory")
 
 args = parser.parse_args()
 
-if not os.path.isfile(args.font_path):
-    sys.exit(f"{args.font_path} is not a file")
+for path in args.fonts_path:
+    if not path.is_file():
+        sys.exit(f"{args.fonts_path} is not a file.")
 
-font = fontforge.open(args.font_path)
+if not args.output_dir.is_dir():
+    sys.exit(f"{args.output_dir} is not a directory.")
 
-with open("unicode.txt", "r") as file:
-    unicodes = [int(u) for u in file.read().strip().split("\n")]
+if args.to_delete.is_file():
+    with args.to_delete.open() as file:
+        unicodes = [int(u) for u in file.read().strip().split("\n")]
+else:
+    sys.exit(f"{args.to_delete} is not a file")
 
-for unicode in unicodes:
-    font.selection.select(("unicode", None), unicode)
-    font.clear()
+for path in args.fonts_path:
+    font = fontforge.open(str(path))
 
-# print(list(font.selection))
-font.generate("build/output.ttf", flags=('opentype'))
+    for unicode in unicodes:
+        font.selection.select(("unicode", None), unicode)
+        font.clear()
+
+    file_name = str(path).split("/")[-1]
+    font.generate(f"build/{file_name}", flags=('opentype'))
 
